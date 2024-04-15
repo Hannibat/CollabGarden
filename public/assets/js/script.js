d276c9a5f25912557efada20c1cbda71b3167150
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-04-15 12:01:46 +0200

    Map

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,40 +1,24 @@
 const getResults = async (url) => {
   const response = await fetch(url);
   const datas = await response.json();
-  console.log(datas);
-  //   return datas;
+  addMapMarkers(datas);
 };
 
 const init = async () => {
   await getResults(
-    "https://opendata.agencebio.org/api/gouv/operateurs/?activite=Production&produit=graines"
+    "https://opendata.agencebio.org/api/gouv/operateurs/?activite=production&produit=graines"
   );
 };
 
-document.addEventListener("DOMContentLoaded", init);
-
-var map = L.map("map").setView([49.8904903, 2.3016024], 13);
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-
-function geoFindMe() {
+const findUserGeo = () => {
   const status = document.querySelector("#status");
-  const mapLink = document.querySelector("#map-link");
-
-  mapLink.href = "";
-  mapLink.textContent = "";
 
   function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
     status.textContent = "";
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    mapLocalisation(latitude, longitude);
   }
 
   function error() {
@@ -47,6 +31,25 @@ function geoFindMe() {
     status.textContent = "Locating…";
     navigator.geolocation.getCurrentPosition(success, error);
   }
-}
+};
 
-document.querySelector("#find-me").addEventListener("click", geoFindMe);
+// Map
+const mapLocalisation = (latitude, longitude) => {
+  let map = L.map("map").setView([latitude, longitude], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+};
+
+const addMapMarkers = (datas) => {
+  console.log(datas);
+  //   producers.forEach((item) => {
+  //     console.log(item.adresseOperateur);
+  //   });
+};
+
+document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", findUserGeo);
