7d710907f74b06d9719a1047776367a4a803b85f
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-04-15 10:29:25 +0200

    DataB

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,14 @@
+const getResults = async (url) => {
+  const response = await fetch(url);
+  const datas = await response.json();
+  console.log(datas);
+  //   return datas;
+};
 
+const init = async () => {
+  await getResults(
+    "https://opendata.agencebio.org/api/gouv/operateurs/?activite=Production&produit=graines"
+  );
+};
+
+document.addEventListener("DOMContentLoaded", init);
