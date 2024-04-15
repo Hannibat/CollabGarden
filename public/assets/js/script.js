2fe85cbb8cfbfd025009a66d737f81a2e7ceca93
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-04-15 14:37:32 +0200

    Markers

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,9 @@
-const getResults = async (url) => {
-  const response = await fetch(url);
-  const datas = await response.json();
-  addMapMarkers(datas);
-};
-
-const init = async () => {
-  await getResults(
-    "https://opendata.agencebio.org/api/gouv/operateurs/?activite=production&produit=graines"
+const getResults = async (latitude, longitude) => {
+  const response = await fetch(
+    `https://opendata.agencebio.org/api/gouv/operateurs/?q=graines&activite=Production&lat=${latitude}&lng=${longitude}&nb=100`
   );
+  const datas = await response.json();
+  getGeo(datas, latitude, longitude);
 };
 
 const findUserGeo = () => {
@@ -18,7 +14,8 @@ const findUserGeo = () => {
     const longitude = position.coords.longitude;
 
     status.textContent = "";
-    mapLocalisation(latitude, longitude);
+    getResults(latitude, longitude);
+    // mapLocalisation(latitude, longitude);
   }
 
   function error() {
@@ -34,7 +31,19 @@ const findUserGeo = () => {
 };
 
 // Map
-const mapLocalisation = (latitude, longitude) => {
+// const mapLocalisation = (latitude, longitude) => {
+//   let map = L.map("map").setView([latitude, longitude], 13);
+
+//   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     maxZoom: 19,
+//     attribution:
+//       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//   }).addTo(map);
+
+//   addMapMarkers(map);
+// };
+
+const getGeo = (datas, latitude, longitude) => {
   let map = L.map("map").setView([latitude, longitude], 13);
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -42,14 +51,26 @@ const mapLocalisation = (latitude, longitude) => {
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
+
+  let producers = datas.items;
+  console.log(producers);
+  producers.forEach((producer) => {
+    let lat = producer.adressesOperateurs[0].lat;
+    let long = producer.adressesOperateurs[0].long;
+    let adress = producer.adressesOperateurs[0].lieu;
+    let city = producer.adressesOperateurs[0].ville;
+    let postalCode = producer.adressesOperateurs[0].codePostal;
+    let marker = L.marker([lat, long]).addTo(map);
+    if (producer.siteWebs.length != 0) {
+      marker.bindPopup(
+        `<b>${producer.denominationcourante}</b><br>${adress}<br>${city} ${postalCode}<br>Site:<a href="${producer.siteWebs[0].url}">Lien</a>`
+      );
+    } else {
+      marker.bindPopup(
+        `<b>${producer.denominationcourante}</b><br>${adress}<br>${city} ${postalCode}`
+      );
+    }
+  });
 };
 
-const addMapMarkers = (datas) => {
-  console.log(datas);
-  //   producers.forEach((item) => {
-  //     console.log(item.adresseOperateur);
-  //   });
-};
-
-document.addEventListener("DOMContentLoaded", init);
 document.addEventListener("DOMContentLoaded", findUserGeo);
